a243d511345e43ae3189152db56f683deab9bda4
Sherry Lam <102286701+sherrylam319@users.noreply.github.com>
2023-02-19 19:07:26 +0800

    add store test

--- tests/integration/store_test.py
+++ tests/integration/store_test.py
@@ -1,16 +1,17 @@
+import json
 
-def test_get_store(client, created_store_id):
+def test_get_store(client, create_store_id):
     response = client.get(
-        f"/store/{created_store_id}"
+        f"/store/{create_store_id[0]}"
     )
     assert response.json == {"id": 1, "location": "fanling", "name": "store_1", "product": {}}
     assert response.status_code == 200
 
 
-def test_delete_store(client, created_store_id):
+def test_delete_store(client, create_store_id):
 
     response = client.delete(
-        f"/store/{created_store_id}"
+        f"/store/{create_store_id[0]}"
     )
     assert response.status_code == 200
     assert response.json == {"message": "Store deleted"}
@@ -20,15 +21,14 @@ def test_post_store(client):
 
     response = client.post(
         "/store",
-        json={"location": "fanling"
-            , "name": "store_1"}
+        json={"location": "fanling",
+               "name": "store_1"}
     )
     assert response.status_code == 200
     assert response.json["id"] == 1
     assert response.json["location"] == "fanling"
     assert response.json["name"] == "store_1"
     assert response.json["product"] == {}
-
 
 
 def test_integrity_error(client):
@@ -44,6 +44,29 @@ def test_integrity_error(client):
 
     assert response.status_code == 400
     assert response.json["message"] == "A store with that name already exists."
+
+
+def test_get_all_store(client):
+    client.post(
+        "/store",
+        json={"location": "fanling", "name": "store_1"}
+    )
+    client.post(
+        "/store",
+        json={"location": "kowloon", "name": "store_2"}
+    )
+
+    response = client.get(
+        "/store"
+    )
+    assert response.json == [{"id": 1, "location": "fanling", "name": "store_1", "product": {}},
+                             {"id": 2, "location": "kowloon", "name": "store_2", "product": {}}]
+
+
+
+
+
+
 
 
 
